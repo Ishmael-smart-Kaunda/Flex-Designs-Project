6ed883cc8a11d1f8f3f57b15b9a13e6fd44ce27a
Ishmael-smart-kaunda <ishmaelsmart003@gmail.com>
2026-02-02 15:48:07 -0800

    changed case on valuation

--- src/pages/valuation.jsx
+++ src/pages/valuation.jsx
@@ -1,21 +1,13 @@
 import Navbar from "../components/home components/Navbar"
 
 import Valuation from "../components/services components/valuation"
-import Management from "../components/services components/management"
-import InvestimentAppraisal from "../components/services components/appraisal"
-import Development from "../components/services components/development"
 import Footer from "../components/Footer"
-
-import CTA from "../components/home components/CTA"
 
 export default function ValuationPage(){
                 return (
                         <>
                          <Navbar/>
                          <Valuation/>
-                         {/*<Management/>
-                         <InvestimentAppraisal/>
-                         <Development/>*/}
                          <Footer/>
                         </>
                 )
