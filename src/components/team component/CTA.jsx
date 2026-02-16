3de011972184fd121ceb0f810ed3f8c6aac530e5
Ishmael-smart-kaunda <ishmaelsmart003@gmail.com>
2026-02-16 14:57:47 -0800

    minor

--- src/components/team component/CTA.jsx
+++ src/components/team component/CTA.jsx
@@ -8,7 +8,7 @@ export default function CTA(){
                         From decision to long-term outcomes, our team provides clear guidance, technical insight, and
                         professional support to help you navigate property investiment journey with confidence.
                     </p>
-                    <Link to="/contacy">
+                    <Link to="/contact">
                        <a className="font-bold border border-blue-900/40 m-auto w-full md:w-fit text-center rounded-md p-2 px-15 border-slate-950/20 text-blue-950">Talk to Us</a>
                     </Link>
                 </div>
